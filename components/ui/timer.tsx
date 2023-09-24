d310bbae9447c5bb6bcf410149cca40e18381c7b
dengi <mcmorgan6560@gmail.com>
2023-09-24 14:49:42 +0100

    reverted

--- components/ui/timer.tsx
+++ components/ui/timer.tsx
@@ -1,39 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import Countdown from 'react-countdown-simple';
+'use client'
+
+import React from 'react'
+import Countdown from "react-countdown-simple";
+
+
+
+
+import { Unica_One } from 'next/font/google'
+
+
+const unica = Unica_One({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 const Timer = () => {
-  // Function to calculate the target date 24 hours from now
-  const calculateTargetDate = () => {
-    const targetDate = new Date();
-    targetDate.setHours(targetDate.getHours() + 24);
-    return targetDate.toISOString();
-  };
 
-  // State to store the target date
-  const [targetDate, setTargetDate] = useState(() => {
-    // Try to retrieve the target date from localStorage
-    const storedTargetDate = localStorage.getItem('targetDate');
-    if (storedTargetDate) {
-      return storedTargetDate;
-    } else {
-      // If not found in localStorage, calculate and set a new target date
-      const newTargetDate = calculateTargetDate();
-      localStorage.setItem('targetDate', newTargetDate);
-      return newTargetDate;
-    }
-  });
-
-  // Update the target date in localStorage when the component mounts
-  useEffect(() => {
-    localStorage.setItem('targetDate', targetDate);
-  }, [targetDate]);
+  const duration = new Date(
+    new Date().setHours(new Date().getHours() + 24)
+  ).toISOString();
 
   return (
     <div>
-      <p className='text-[48px] md:text-[64px] text-white'>
+      <p style={unica.style} className='text-[48px] md:text-[64px] text-white'>
         <span>
           <Countdown
-            targetDate={targetDate}
+            targetDate={duration}
             renderer={({ hours, minutes, seconds }) => (
               <div className='text-white'>
                 {hours}<span className='text-base'>H</span> {minutes}<span className='text-base'>M</span> {seconds}<span className='text-base'>S</span>
@@ -43,7 +36,7 @@ const Timer = () => {
         </span>
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default Timer;
+export default Timer
